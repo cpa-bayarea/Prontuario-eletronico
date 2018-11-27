110e88dee19365327e3220637b3bbfb68e8ff53a
Douglasx <douglasantana007@gmail.com>
2018-11-27 07:03:15 -0200

    [FIX] Infraestrutura do Projeto.
    Não versionados arquivos que tornem o sistema comerciável.

---
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email', 60)->unique();
+            $table->string('username', 11)->unique();
+
+            $table->integer('id_perfil')->unsigned();
+            $table->foreign('id_perfil')->references('id_perfil')->on('tb_perfil');
+
+            $table->char('status', 1)->default('P'); // options => [A, I, P] Active, Inative or Pending
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
+
